93d122fff48187f9681b5fcb1ee9e2aee6852ac0
mykolazemko <bob@>
2022-07-02 14:02:11 +0300

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 
 import './App.scss';
+import Header from './Components/Header/Header';
 import { Characters } from './Pages/Characters/Characters';
 import { Route, Routes } from 'react-router';
 import { CharacterInfo } from './Pages/CharacterInfo/CharacterInfo';
 import Page_404 from './Components/Page_404/Page_404';
-import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 
 function App() {
