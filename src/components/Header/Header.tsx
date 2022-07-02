081477eb4c00958a65babdb9ced36720d7871298
mykolazemko <bob@>
2022-07-02 13:27:26 +0300

    add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,22 +4,18 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { searchCharacterByName } from "../../Redux/CharacterSlice";
 import { SearchModule } from "../SearchModule/SearchModule";
+import {ReactComponent as Logo} from "../../logo.svg";
 
 const Header = () => {
-  const [character, setCharacter] = useState("");
-
-  const dispatch = useDispatch();
-  const handleSubmit = (e: any) => {
-    dispatch(searchCharacterByName(character))
-    e.preventDefault();
-  }
 
   return (
     <div className={style.header}>
       {/* <Link style={{ textDecoration: 'none' }} to="/"> */}
       <span className={style.header__menuItem}>
         {/* <h3>Characters</h3> */}
-        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>
+        <Logo />
+        </Link>
       </span>
       <span className={style.header__menuItem}>
         <Link to={"/characters"}>Characters</Link>
